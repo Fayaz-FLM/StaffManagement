b01e853fa0d5fb05618602810ba5b8116117353d
dade-divya <divyadade444@gamil.com>
2026-09-20 08:50:28 +0530

    Added Annotation for dao files

--- src/main/java/com/flm/staff/dao/StaffAddressRepository.java
+++ src/main/java/com/flm/staff/dao/StaffAddressRepository.java
@@ -1,9 +1,10 @@
 package com.flm.staff.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.flm.staff.entity.StaffAddress;
 
+@Repository
 public interface StaffAddressRepository extends JpaRepository<StaffAddress, Integer> {
 
 }
